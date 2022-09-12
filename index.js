b36e05c8fbee8fb830f15840b973a7bcafe36eed
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2022-09-12 16:04:39 +0100

    feat: add CORS different types

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const cors = require('cors');
 
 app.use(express.static('public'));
 
-app.use(cors());
+var corsOptions = {
+  origin: 'https://producthackers.com',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-//index.js
-app.get('/', (req, res) => {
-  // res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+// No CORS
+app.get('/no-cors', (req, res) => {
+  res.json({msg: 'This is CORS-disabled!'})
+});
+
+// All CORS Origins
+app.get('/all-cors', cors(), (req, res) => {
   res.json({msg: 'This is CORS-enabled for all origins!'})
+});
+
+// CORS enabled for PH Website
+app.get('/ph-cors', cors(corsOptions), (req, res) => {
+  res.json({msg: 'This is CORS-enabled for PH Origins!'})
 });
 
 app.listen(process.env.PORT || 3000);
