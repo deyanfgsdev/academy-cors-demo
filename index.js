87f1607636060414d1cc9281e2085f96d5edc15a
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2022-09-12 15:54:41 +0100

    checking cors

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 app.use(express.static('public'));
 
+app.use(cors());
+
 //index.js
 app.get('/', (req, res) => {
-  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  // res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  res.json({msg: 'This is CORS-enabled for all origins!'})
 });
 
 app.listen(process.env.PORT || 3000);
