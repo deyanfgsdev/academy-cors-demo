8c4bd42ec0f341a54119eb2bbdaf51ce23cbe8c6
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2022-09-12 15:38:49 +0100

    fix: fixing deploy

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 8080;
 
-app.use(cors());
+app.use(express.json({ extended: false }));
+// app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World 2!')
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 });
